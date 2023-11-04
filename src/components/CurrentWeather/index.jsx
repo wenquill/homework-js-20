ff08c8d0ef0896236327b60e1bb9ac6052027d8f
victoria <vikabirukova22@gmail.com>
2023-11-04 23:18:10 +0200

    add all functionality

--- src/components/CurrentWeather/index.jsx
+++ src/components/CurrentWeather/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { loadWeather } from "../../api";
 import { FadeLoader } from "react-spinners";
 import styles from "./CurrentWeather.module.css";
+import { LiaTemperatureLowSolid, LiaWindSolid } from "react-icons/lia";
 
 function CurrentWeather() {
   const [weather, setWeather] = useState(null);
@@ -15,7 +16,6 @@ function CurrentWeather() {
       .then((data) => {
         setWeather(data);
         console.log(data);
-        console.log(weather);
       })
       .catch((err) => console.error(err))
       .finally(() => setIsFetching(false));
@@ -30,26 +30,52 @@ function CurrentWeather() {
   }
 
   return (
-    <>
+    <main className={styles.main}>
       <section className={styles.selectUnit}>
-        <label>
-          Виберіть одиницю вимірювання температури:
-          <select value={temperature_unit} onChange={selectTempUnitHandler}>
+        <label className={styles.label}>
+          <p className={styles.unitName}>Temperature unit</p>
+          <select
+            className={styles.select}
+            value={temperature_unit}
+            onChange={selectTempUnitHandler}
+          >
             <option value="fahrenheit">*F</option>
             <option value="celsius">*C</option>
           </select>
         </label>
-        <label>
-          Виберіть одиницю вимірюванння швидкості вітру:
-          <select value={windspeed_unit} onChange={selectWindUnitHandler}>
+        <label className={styles.label}>
+          <p className={styles.unitName}>Windspeed unit</p>
+          <select
+            className={styles.select}
+            value={windspeed_unit}
+            onChange={selectWindUnitHandler}
+          >
             <option value="kmh">km/h</option>
             <option value="ms">m/s</option>
           </select>
         </label>
       </section>
-      <div className={styles.loader}>{isFetching && <FadeLoader />}</div>
-      <div></div>
-    </>
+      <section className={styles.currWeatherUnit}>
+        <div className={styles.loader}>{isFetching && <FadeLoader color="white"/>}</div>
+        {!isFetching && (
+          <>
+            <h2>Current weather</h2>
+            <div>
+              <div className={styles.weatherParams}>
+                <LiaWindSolid />
+                {weather && weather.current.windspeed_10m}{" "}
+                {weather && weather.current_units.windspeed_10m}
+              </div>
+              <div className={styles.weatherParams}>
+                <LiaTemperatureLowSolid />
+                {weather && weather.current.temperature_2m}{" "}
+                {weather && weather.current_units.temperature_2m}
+              </div>
+            </div>
+          </>
+        )}
+      </section>
+    </main>
   );
 }
 
